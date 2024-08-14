e283321efa646e42a8aaa360f20fef4679f8c0f8
DannyBW23 <dannybw212@gmail.com>
2024-08-14 15:28:04 -0400

    o

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="/favicon.ico?v=2" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
