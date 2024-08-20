ca1976a2c6d1973c035c44d7f697363213fbbf2a
DannyBW23 <dannybw212@gmail.com>
2024-08-20 13:00:51 -0500

    o

--- src/app/PS/page.tsx
+++ src/app/PS/page.tsx
@@ -14,7 +14,7 @@ export default function Component() {
       }, []); 
     return (
         <html>
-        <meta name="viewport" content="width=device-width, initial-scale=.6" />
+        <meta name="viewport" content="width=device-width, initial-scale=.4" />
       <>
      
 <div
